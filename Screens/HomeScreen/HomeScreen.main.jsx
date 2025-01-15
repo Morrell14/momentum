b096b8846d5f90325d662e2de367c52df96ef3a6
Morrell14 <morrellnioble@hotmail.com>
2025-01-15 04:01:09 -0800

    screens set up

---
+++ Screens/HomeScreen/HomeScreen.main.jsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+
+export default function HomeScreen() {
+    return (
+        <View style= {{flex: 1, backgroundColor: "cream"}}>
+            
+            <Text> Hello World </Text>
+
+        </View>
+    );
+}
